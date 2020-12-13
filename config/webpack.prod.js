4c2200984ee057cc8f664531f02f2f50f6490408
cyao <cyao@cybozu.cn>
2020-12-13 11:58:36 +0800

    add css support

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,10 +1,16 @@
 const { resolve } = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = {
   entry: {
-    desktop: resolve(__dirname, '../src/desktop.js'),
-    config: resolve(__dirname, '../src/config.js'),
+    desktop: resolve(__dirname, '../plugin/js/desktop.js'),
+    config: [
+      resolve(__dirname, '../plugin/js/config.js'), 
+      resolve(__dirname, '../plugin/css/config.css')
+    ],
   },
   output: {
     filename: `js/[name].js`,
@@ -19,8 +25,21 @@ module.exports = {
             compress: { pure_funcs: ['console.log'] },
           },
         }),
+        new OptimizeCssAssetsPlugin({
+          cssProcessorPluginOptions: {
+            preset: ['default', { discardComments: { removeAll: true } }],
+          },
+        }),
     ].filter(Boolean),
   },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].css',
+      chunkFilename: 'css/[name].css',
+      ignoreOrder: false,
+    }),
+  ],
   module: {
     rules: [
       {
@@ -28,6 +47,42 @@ module.exports = {
         loader: 'babel-loader',
         options: { cacheDirectory: true },
         exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              modules: false,
+              sourceMap: false,
+              importLoaders: 1,
+            },
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [
+                  'postcss-flexbugs-fixes',
+                  [
+                    'postcss-preset-env',
+                    {
+                      autoprefixer: {
+                        grid: true,
+                        flexbox: 'no-2009',
+                      },
+                      stage: 3,
+                    },
+                  ],
+                  'postcss-normalize',
+                ],
+              },
+              sourceMap: false,
+            },
+          },
+        ],
       },
     ],
   },
